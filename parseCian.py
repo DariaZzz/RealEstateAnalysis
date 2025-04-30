9b356518c6108b346840df66fb7d31edf30dcb46
Zakharova_DR <dashikzaharova@yandex.ru>
2025-04-30 20:08:18 +0300

    Added flat_dict as Parser's field for handy storage, description of the dict

--- parseCian.py
+++ parseCian.py
@@ -9,6 +9,21 @@ class Parser(object):
         self.page = ''
         self.page_num = 2
         self.url = f"https://www.cian.ru/cat.php?currency=2&deal_type=sale&engine_version=2&maxprice=20000000&offer_type=flat&{self.page}region=1&room1=1&room2=1"
+        '''
+        flat_dict:
+            url - url квартиры
+            price - стоимость квартиры (в рублях)
+            underground - метро:
+                name - название
+                travel_type - тип передвижения до метро (walk, transport)
+                travel_time - время передвижения до метро (в минутах)
+            total_area - общая площадь (в м кв.)
+            living_area - жилая площадь (в м кв.)
+            floor - этаж
+            kitchen_area - площадь кухни (в м кв.)
+            year - год постройки или сдачи
+            housing_type - тип жилья(Новостройка, Вторичка, Апартаменты) или комбинации (Новостройка / Апартаменты)
+        '''
         self.flat_dict = {}
 
     # парсинг отдельной квартиры
@@ -16,6 +31,7 @@ class Parser(object):
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         print(f"URL: {url}")
+        page_dict = {}
 
         script_tag = [s for s in soup.find_all("script") if s.get("type") == "application/ld+json"][0]
         if not script_tag:
@@ -25,6 +41,7 @@ class Parser(object):
             if price_match:
                 price = int(price_match.group(1))
                 print(f"Значение price: {price}")
+                page_dict['price'] = price # собираем dict страницы
 
         script_tag = soup.find("script", string=re.compile(r'"undergrounds":\s*\[.*?\]'))
         undergrounds_match = re.findall(r'"undergrounds":\s*\[.*?\]', str(script_tag), re.DOTALL)[0]
@@ -35,6 +52,8 @@ class Parser(object):
             print("Извлеченные данные:")
             for name, travel_type, travel_time in matches:
                 print(f"- Название: {name}, Тип передвижения: {travel_type}, Время в пути: {travel_time} минут")
+            name, travel_type, travel_time = matches[0]
+            page_dict['underground'] = {'name': name, 'travel_type': travel_type, 'travel_time': int(travel_time)} # собираем dict страницы
         else:
             print("Данные не найдены.")
 
@@ -56,37 +75,57 @@ class Parser(object):
 
                 # Извлечение значений площади
                 for factoid in factoids:
-                    # total_area = None
-                    # living_area = None
-                    # floor = None
-                    # kitchen_area = None
-                    # built_year = None
-                    # completion_year = None
                     if factoid["title"] == "Общая площадь":
                         total_area = factoid["value"]
-                        print(f"Общая площадь: {total_area}")
+                        print(f"Общая площадь: {total_area[:-3]}")
+                        page_dict['total_area'] = float(total_area[:-3].replace(',', '.')) # собираем dict страницы
                     elif factoid["title"] == "Жилая площадь":
                         living_area = factoid["value"]
                         print(f"Жилая площадь: {living_area}")
+                        page_dict['living_area'] = float(living_area[:-3].replace(',', '.')) # собираем dict страницы
                     elif factoid["title"] == "Этаж":
                         floor = factoid["value"]
                         print(f"Этаж: {floor}")
+                        page_dict['floor'] = floor # собираем dict страницы
                     elif factoid["title"] == "Площадь кухни":
                         kitchen_area = factoid["value"]
-                        print(f"Этаж: {kitchen_area}")
+                        print(f"Площадь кухни: {kitchen_area}")
+                        page_dict['kitchen_area'] = float(kitchen_area[:-3].replace(',', '.')) # собираем dict страницы
                     elif factoid["title"] == "Год постройки":
                         built_year = factoid["value"]
                         print(f"Год постройки: {built_year}")
+                        page_dict['year'] = int(built_year) # собираем dict страницы
                     elif factoid["title"] == "Год сдачи":
                         completion_year = factoid["value"]
                         print(f"Год сдачи: {completion_year}")
+                        page_dict['year'] = int(completion_year) # собираем dict страницы
             else:
                 print("Не удалось найти блок с 'factoids'.")
         else:
             print("Не удалось найти тег <script> с данными.")
-        print("*" * 100)
 
-    # парсинг главной страницы
+        # поиск типа жилья
+        items = soup.find_all('div', {'data-name': 'OfferSummaryInfoItem'})
+        for item in items:
+            key = item.find('p')
+            if key and 'Тип жилья' in key.get_text(strip=True):
+                value = key.find_next('p')
+                if value:
+                    housing_type = value.get_text(strip=True)
+                    print("Тип жилья:", housing_type)
+                    page_dict['housing_type'] = housing_type
+                    # match housing_type:
+                    #     case "Новостройка":
+                    #         page_dict['housing_type'] = 0
+                    #     case "Вторичка":
+                    #         page_dict['housing_type'] = 1
+                    #     case "Апартаменты":
+                    #         page_dict['housing_type'] = 2
+                    break
+        print('*' * 100)
+        self.flat_dict[url] = page_dict
+
+    # парсинг страницы
     def parse_page(self):
         response = requests.get(self.url)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -94,6 +133,7 @@ class Parser(object):
         for a in all_links:
             self.parse_flat_info(a['href'])
 
+    #парсинг указанного числа страниц
     def parse_pages(self, number_of_parsing=20):
         for i in range(number_of_parsing):
             self.parse_page()
@@ -104,4 +144,5 @@ class Parser(object):
                 self.page_num += 1
 
 parser = Parser()
-parser.parse_pages(2)
+parser.parse_pages(1)
+print(parser.flat_dict)
