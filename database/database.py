78a3e1778fe8513c0e0e0411fd02331a2923162d
Nochninog <vorotnikova.elizaveta@internet.ru>
2025-05-25 20:59:34 +0300

    fix database

--- database/database.py
+++ database/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import URL, create_engine, text
-from config import settings
+from database.config import settings
 
 engine = create_engine(
     url=settings.DATABASE_URL_psycopg,
