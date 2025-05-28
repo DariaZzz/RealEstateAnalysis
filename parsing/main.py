d262af758873749206b9cc3237e890f8a67e7608
Zakharova_DR <dashikzaharova@yandex.ru>
2025-05-28 17:10:15 +0300

    changed imoorts and added db filling

--- parsing/main.py
+++ parsing/main.py
@@ -1,11 +1,9 @@
-# from database.core import process_flat_data, process_flats_dict
-from Parser import Parser
+from database.core import process_flats_dict
+from parsing.Parser import Parser
 
 # docker-compose build parser
 # docker-compose up -d parser
 
 parser = Parser()
 parser.parse_pages(1, logging=True)
-# process_flats_dict(parser.flat_dict)
-# print(parser.flat_dict)
-# make_json(parser.flat_dict)
+process_flats_dict(parser.flat_dict)
